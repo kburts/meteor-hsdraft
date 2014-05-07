3d49f981cebfed1ba5f67c5eb56e70b2bded61ac
kburts <kevin.burton2@gmail.com>
2014-05-06 22:33:52 -0700

    Made the heroes work (heroes added to the DB, then fetched from the listHeroes template

---
+++ hsdraft/client/views/heroes/heroes_list.js
@@ -0,0 +1,43 @@
+var heroesData = [
+  {
+    Name: 'Hunter',
+    Picture: ''
+  },
+  {
+    Name: 'Druid',
+    Picture: '' 
+  },
+  {
+    Name: 'Rogue',
+    Picture: ''   
+
+  },
+  {
+    Name: 'Warrior',
+    Picture: ''   
+  },
+  {
+    Name: 'Priest',
+    Picture: '' 
+  },
+  {
+    Name: 'Shaman',
+    Picture: '' 
+  },
+  {
+    Name: 'Warlock',
+    Picture: '' 
+  },
+  {
+    Name: 'Mage',
+    Picture: '' 
+  },
+  {
+    Name: 'Paladin',
+    Picture: '' 
+  }
+]
+
+Template.heroesList.helpers({
+  heroes: heroesData
+})
--- hsdraft/hsdraft.js
+++ hsdraft/hsdraft.js
@@ -1,20 +1,25 @@
-Questions = new Meteor.Collection('Questions');
+Heroes = new Meteor.Collection('heroes');
 
 if (Meteor.isClient) {
-  Template.question.allQuestions = function() {
-    return Questions.find();
-  };  
-
-  Template.questions.events({
-    "click #questionAsk": function(evt, templ) {
-      var question = templ.find("#questionText").value;
-      Questions.insert({question: question, score: 1});
-    }  
-  });
+  Template.heroesList.heroes = function () {
+    return Heroes.find({}, {sort: {score: -1, hero: 1}});
+  };
 }
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
-    // code to run on server at startup
+    if (Heroes.find().count() === 0) {
+      var heroes = ["Hunter",
+                   "Rogue",
+                   "Paladin",
+                   "Priest",
+                   "Mage",
+                   "Warlock",
+                   "Druid",
+                   "Shaman",
+                   "Warrior"];
+      for (var i = 0; i < heroes.length; i++)
+        Heroes.insert({name: heroes[i], score: Math.floor(Random.fraction()*10)*5});
+    }
   });
 }
